d89ecb41399f272ef024e0f48f07e236b9d1bf0e
chetangour_cap <chetangour2017@gmail.com>
2021-12-17 13:53:45 +0530

    Routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './MyComponents/about/about.component';
+import { HomeComponent } from './MyComponents/home/home.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent }
   ];
 
 @NgModule({
